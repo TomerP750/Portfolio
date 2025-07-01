ed043f8ce8982bc7e410c91c14d22100b94a7f8b
TomerP750 <tomer.jcb65@gmail.com>
2025-07-01 17:04:41 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import {Layout} from "./Components/Layout/Layout.tsx";
 import {BrowserRouter} from "react-router-dom";
 
 createRoot(document.getElementById('root')!).render(
-    <BrowserRouter>
+    <BrowserRouter basename="/Portfolio">
         <Layout/>
     </BrowserRouter>,
 )
