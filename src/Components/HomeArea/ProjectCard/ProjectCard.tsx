8a2382cb3a962c76b8d18d052ac5f5a6962d5d1e
TomerP750 <tomer.jcb65@gmail.com>
2025-07-09 13:59:59 +0300

    navigation bug fixed

--- src/Components/HomeArea/ProjectCard/ProjectCard.tsx
+++ src/Components/HomeArea/ProjectCard/ProjectCard.tsx
@@ -1,20 +1,20 @@
-import {JSX, useState} from "react";
+import { JSX, useState } from "react";
 import "./ProjectCard.css";
-import {Project} from "../../../Models/Project.ts";
-import {ArrowRight} from "lucide-react";
-import {FiExternalLink} from "react-icons/fi";
-import {FaBookOpen, FaGithub} from "react-icons/fa";
-import {ProjectType} from "../../../Models/ProjectType.ts";
+import { Project } from "../../../Models/Project.ts";
+import { ArrowRight } from "lucide-react";
+import { FiExternalLink } from "react-icons/fi";
+import { FaBookOpen, FaGithub } from "react-icons/fa";
+import { ProjectType } from "../../../Models/ProjectType.ts";
 import { AnimatePresence, motion, Variants } from "framer-motion";
-import { useNavigate } from "react-router-dom";
-import {Button} from "../../Wrappers/Button/Button.tsx";
+import { NavLink, useNavigate } from "react-router-dom";
+import { Button } from "../../Wrappers/Button/Button.tsx";
 
 
 interface ProjectCardProps {
     project: Project
 }
 
-export function ProjectCard({project}: ProjectCardProps): JSX.Element {
+export function ProjectCard({ project }: ProjectCardProps): JSX.Element {
 
     const githubDisabled = !project.gitHubUrl?.trim();
     const webUrlDisabled = !project.webUrl?.trim();
@@ -22,7 +22,7 @@ export function ProjectCard({project}: ProjectCardProps): JSX.Element {
     const navigate = useNavigate();
 
     const containerVariants: Variants = {
-        hidden: { },
+        hidden: {},
         show: {
             transition: {
                 staggerChildren: 0.1,
@@ -33,7 +33,7 @@ export function ProjectCard({project}: ProjectCardProps): JSX.Element {
 
     const itemVariants: Variants = {
         hidden: { opacity: 0, y: 20 },
-        show:   { opacity: 1, y: 0, transition: { duration: 0.4 } }
+        show: { opacity: 1, y: 0, transition: { duration: 0.4 } }
     };
 
     const truncatedContent =
@@ -42,84 +42,92 @@ export function ProjectCard({project}: ProjectCardProps): JSX.Element {
             : project.content;
 
 
+
     return (
         <>
             <div className={`w-full h-164 flex flex-col items-center border border-[#10b981]`}>
                 {project.imageUrl ? <div className="relative w-full h-auto group flex items-center justify-center"
-                         onMouseOver={()=>setImageHovered(true)}
-                         onMouseLeave={()=>setImageHovered(false)}>
-                        <img
-                            src={project.imageUrl}
-                            alt="image"
-                            className={`block w-full h-auto ${imageHovered ? "pointer-events-none" : ""}`}
-                        />
-                        {imageHovered && <AnimatePresence>
-                            {imageHovered && (
+                    onMouseOver={() => setImageHovered(true)}
+                    onMouseLeave={() => setImageHovered(false)}>
+                    <img
+                        src={project.imageUrl}
+                        alt="image"
+                        className={`block w-full h-auto ${imageHovered ? "pointer-events-none" : ""}`}
+                    />
+                    {imageHovered && <AnimatePresence>
+                        {imageHovered && (
+                            <motion.div
+                                className="absolute inset-0 sm:px-10 flex items-center justify-around bg-black/60 z-10"
+                                variants={containerVariants}
+                                initial="hidden"
+                                animate="show"
+                                exit="hidden"
+                            >
                                 <motion.div
-                                    className="absolute inset-0 sm:px-10 flex items-center justify-around bg-black/60 z-10"
-                                    variants={containerVariants}
-                                    initial="hidden"
-                                    animate="show"
-                                    exit="hidden"
+                                    className="flex flex-col items-center text-white hover:text-[#10b981] cursor-pointer"
+                                    variants={itemVariants}
+                                    onClick={() => navigate(`/project/${project.id}`)}
                                 >
-                                    <motion.div
-                                        className="flex flex-col items-center text-white hover:text-[#10b981] cursor-pointer" variants={itemVariants}
-                                        onClick={() => navigate(`/project/${project.id}`)}
-                                    >
-                                        <FaBookOpen
-                                            size={25}
+                                    <FaBookOpen
+                                        size={25}
 
-                                            className=""
-                                        />
-                                        <p>Learn More</p>
-                                    </motion.div>
-                                    <motion.div variants={itemVariants}>
+                                        className=""
+                                    />
+                                    <p>Learn More</p>
+                                </motion.div>
+                                <motion.div variants={itemVariants}>
+                                    <a href={project.gitHubUrl} target="_blank" rel="noopener noreferrer">
                                         <Button disabled={githubDisabled}
-                                                Icon={FaGithub}
-                                                size={25}
-                                                onClick={() => navigate(`${project.gitHubUrl}`)}
-                                                className={"flex flex-col text-white items-center hover:text-[#10b981] cursor-pointer disabled:cursor-not-allowed disabled:text-white/50"}
+                                            Icon={FaGithub}
+                                            size={25}
+                                            className={"flex flex-col text-white items-center hover:text-[#10b981] cursor-pointer disabled:cursor-not-allowed disabled:text-white/50"}
                                         >Github</Button>
-                                    </motion.div>
-                                    <motion.div variants={itemVariants}>
+                                    </a>
+                                </motion.div>
+                                <motion.div variants={itemVariants}>
+                                    <a href={project.webUrl} target="_blank" rel="noopener noreferrer">
                                         <Button
                                             disabled={webUrlDisabled}
                                             Icon={FiExternalLink}
                                             size={25}
-                                            onClick={() => navigate(`${project.webUrl}`)}
                                             className={"flex flex-col text-white items-center hover:text-[#10b981] cursor-pointer disabled:cursor-not-allowed disabled:text-white/50"}
                                         >
                                             Website
                                         </Button>
-                                    </motion.div>
+                                    </a>
                                 </motion.div>
-                            )}
-                        </AnimatePresence>}
-                    </div>
+                            </motion.div>
+                        )}
+                    </AnimatePresence>}
+                </div>
 
 
-                    : <div className="w-full bg-[#10b981] h-4/5"/>}
+                    : <div className="w-full bg-[#10b981] h-4/5" />}
                 <div className={"border-t border-[#10b981] flex flex-col items-center h-full w-full"}>
 
                     <div className="flex flex-col items-start sm:flex-row sm:justify-between mt-5 w-9/10">
-                        <p className="mb-4 font-semibold text-2xl text-white underline underline-offset-8">{project.id}) {project.title}</p>
+                        <p className="mb-4 font-semibold text-2xl text-white underline underline-offset-8">{project.id} {project.title}</p>
                         <div className="flex gap-4 mb-4 sm:mb-0">
-                            <button disabled={githubDisabled}
+                            <a href={project.gitHubUrl} target="_blank" rel="noopener noreferrer">
+                                <button disabled={githubDisabled}
                                     className={"disabled:cursor-not-allowed " +
                                         "disabled:text-white/50 text-white hover:text-[#8257e5] cursor-pointer"}>
-                                <FaGithub
-                                    size={25}
-                                    className="transition duration-200 ease-in"
-                                />
-                            </button>
-                            <button disabled={webUrlDisabled}
+                                    <FaGithub
+                                        size={25}
+                                        className="transition duration-200 ease-in"
+                                    />
+                                </button>
+                            </a>
+                            <a href={project.webUrl} target="_blank" rel="noopener noreferrer">
+                                <button disabled={webUrlDisabled}
                                     className={"disabled:cursor-not-allowed " +
                                         "disabled:text-white/50 text-white hover:text-[#10b981] cursor-pointer"}>
-                                <FiExternalLink
-                                    size={25}
-                                    className="transition duration-200 ease-in"
-                                />
-                            </button>
+                                    <FiExternalLink
+                                        size={25}
+                                        className="transition duration-200 ease-in"
+                                    />
+                                </button>
+                            </a>
                         </div>
                     </div>
                     <div className="flex justify-start w-9/10">
@@ -128,7 +136,7 @@ export function ProjectCard({project}: ProjectCardProps): JSX.Element {
                                 key={p.toString()}
                                 className={`px-2 py-1 text-black text-sm rounded-xl
                             ${p === ProjectType.FULLSTACK ? "bg-[#EEF2FF]" : p === ProjectType.BACKEND ? "bg-[#ECFDF5]" : "bg-[#FFF8E1]"}`}>
-                            {p.toString()}</span>)}
+                                {p.toString()}</span>)}
                     </div>
 
 
@@ -139,9 +147,9 @@ export function ProjectCard({project}: ProjectCardProps): JSX.Element {
                                 <li
                                     key={pl.language}
                                     className="rounded-full px-2 py-1 text-sm font-medium text-white flex items-center gap-2"
-                                    style={{backgroundColor: pl.colorHexCode}}
+                                    style={{ backgroundColor: pl.colorHexCode }}
                                 >
-                                    <Icon size={16} className="text-white"/>
+                                    <Icon size={16} className="text-white" />
                                     {pl.language}
                                 </li>
                             );
@@ -155,11 +163,11 @@ export function ProjectCard({project}: ProjectCardProps): JSX.Element {
                     <div
                         className="flex w-9/10 justify-end mb-8"
                     >
-                    <span
-                        onClick={() => navigate(`/project/${project.id}`)}
-                        className={"flex items-center gap-1 text-[#10b981] hover:opacity-80 transition duration-200 ease-in cursor-pointer"}>
-                        Learn More <ArrowRight/>
-                    </span>
+                        <span
+                            onClick={() => navigate(`/project/${project.id}`)}
+                            className={"flex items-center gap-1 text-[#10b981] hover:opacity-80 transition duration-200 ease-in cursor-pointer"}>
+                            Learn More <ArrowRight />
+                        </span>
                     </div>
                 </div>
             </div>
--- src/Components/HomeArea/ProjectDetailsPage/ProjectDetailsPage.tsx
+++ src/Components/HomeArea/ProjectDetailsPage/ProjectDetailsPage.tsx
@@ -1,25 +1,25 @@
 import "./ProjectDetailsPage.css";
-import {JSX, useEffect} from "react";
-import {Navigate, useLocation, useNavigate, useParams} from "react-router-dom";
-import {FiArrowLeft, FiExternalLink} from "react-icons/fi";
-import {FaGithub} from "react-icons/fa";
-import {projectsData} from "../../../Datas/ProjectsData.ts";
-import {ProjectType} from "../../../Models/ProjectType.ts";
-import {Footer} from "../../Footer/Footer.tsx";
-import {Button} from "../../Wrappers/Button/Button.tsx";
+import { JSX, useEffect } from "react";
+import { Navigate, useLocation, useNavigate, useParams } from "react-router-dom";
+import { FiArrowLeft, FiExternalLink } from "react-icons/fi";
+import { FaGithub } from "react-icons/fa";
+import { projectsData } from "../../../Datas/ProjectsData.ts";
+import { ProjectType } from "../../../Models/ProjectType.ts";
+import { Footer } from "../../Footer/Footer.tsx";
+import { Button } from "../../Wrappers/Button/Button.tsx";
 
 
 export function ProjectDetailsPage(): JSX.Element {
 
-    
+
     const navigate = useNavigate();
-    const {id} = useParams<{ id: string }>();
+    const { id } = useParams<{ id: string }>();
     const project = projectsData.find(p => p.id === Number(id));
     const githubDisabled = !project?.gitHubUrl?.trim();
     const webUrlDisabled = !project?.webUrl?.trim();
-    const {pathname} = useLocation();
+    const { pathname } = useLocation();
     const handleBackToMain = () => {
-        navigate("/", {state: "projects", replace: true});
+        navigate("/", { state: "projects", replace: true });
     }
 
     useEffect(() => {
@@ -27,7 +27,7 @@ export function ProjectDetailsPage(): JSX.Element {
     }, [pathname])
 
     if (!project) {
-        return <Navigate to="/" state={"projects"} replace={true}/>
+        return <Navigate to="/" state={"projects"} replace={true} />
     }
 
     return (
@@ -40,7 +40,7 @@ export function ProjectDetailsPage(): JSX.Element {
                     <div
                         onClick={() => handleBackToMain()}
                         className="flex justify-center items-center gap-2 border border-white hover:border-[#10b981] px-6 py-2 rounded-full cursor-pointer text-sm hover:text-[#10b981] transition duration-300">
-                        <FiArrowLeft/>
+                        <FiArrowLeft />
                         <p>Back to Main Page</p>
                     </div>
 
@@ -54,20 +54,21 @@ export function ProjectDetailsPage(): JSX.Element {
                     <div className="flex flex-col items-center gap-4">
 
                         <div className="flex gap-5">
-
-                            <Button disabled={githubDisabled}
+                            <a href={project.gitHubUrl} target="_blank" rel="noopener noreferrer">
+                                <Button disabled={githubDisabled}
                                     Icon={FaGithub}
                                     size={30}
-                                    onClick={() => navigate(`${project.gitHubUrl}`)}
                                     className={"flex flex-col text-white items-center disabled:cursor-not-allowed disabled:text-white/50 hover:text-[#8257e5] cursor-pointer"}
-                            />
-                            <Button
-                                disabled={webUrlDisabled}
-                                Icon={FiExternalLink}
-                                size={30}
-                                onClick={() => navigate(`${project.webUrl}`)}
-                                className={"flex flex-col text-white items-center disabled:cursor-not-allowed disabled:text-white/50 hover:text-[#10b981] cursor-pointer"}
-                            />
+                                />
+                            </a>
+                            <a href={project.gitHubUrl} target="_blank" rel="noopener noreferrer">
+                                <Button
+                                    disabled={webUrlDisabled}
+                                    Icon={FiExternalLink}
+                                    size={30}
+                                    className={"flex flex-col text-white items-center disabled:cursor-not-allowed disabled:text-white/50 hover:text-[#10b981] cursor-pointer"}
+                                />
+                            </a>
 
                         </div>
                     </div>
@@ -86,7 +87,7 @@ export function ProjectDetailsPage(): JSX.Element {
                                 key={p.toString()}
                                 className={`px-2 py-1 text-black text-sm rounded-xl
                             ${p === ProjectType.FULLSTACK ? "bg-[#EEF2FF]" : p === ProjectType.BACKEND ? "bg-[#ECFDF5]" : "bg-[#FFF8E1]"}`}>
-                            {p.toString()}</span>)}
+                                {p.toString()}</span>)}
                     </section>
 
                     <section className="w-full max-w-3xl space-y-3">
@@ -100,9 +101,9 @@ export function ProjectDetailsPage(): JSX.Element {
                                     <li
                                         key={pl.language}
                                         className="rounded-full px-4 py-1 text-sm font-medium text-white flex items-center gap-2"
-                                        style={{backgroundColor: pl.colorHexCode}}
+                                        style={{ backgroundColor: pl.colorHexCode }}
                                     >
-                                        <Icon size={16} className="text-white"/>
+                                        <Icon size={16} className="text-white" />
                                         {pl.language}
                                     </li>
                                 );
@@ -132,7 +133,7 @@ export function ProjectDetailsPage(): JSX.Element {
 
 
             </div>
-            <Footer page={"projectPage"}/>
+            <Footer page={"projectPage"} />
         </>
     );
 }
