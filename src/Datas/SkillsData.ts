b4367beff72b04b1aa7a80270b9b00f1a8c0a254
TomerP750 <tomer.jcb65@gmail.com>
2026-08-16 10:11:07 +0300

    Update SkillsData.ts

--- src/Datas/SkillsData.ts
+++ src/Datas/SkillsData.ts
@@ -4,6 +4,7 @@ import {
     SiExpress,
     SiHtml5,
     SiJavascript,
+    SiNestjs,
     SiMongodb,
     SiMysql,
     SiNodedotjs,
@@ -21,92 +22,98 @@ export const skills = [
         name: 'Java',
         icon: FaJava,
         color: '#f89820',
-        role: 'backend' as Role 
-        
+        role: 'backend' as Role
+
     },
     {
         name: 'Spring',
         icon: SiSpring,
         color: '#6db33f',
-        role: 'backend' as Role 
+        role: 'backend' as Role
+    },
+    {
+        name: 'NestJS',
+        icon: SiNestjs,
+        color: '#E0234E',
+        role: 'backend' as Role
     },
     {
         name: 'React',
         icon: SiReact,
         color: '#61dafb',
-        role: 'frontend' as Role 
-        
+        role: 'frontend' as Role
+
     },
     {
         name: 'TypeScript',
         icon: SiTypescript,
         color: '#3178c6',
-        role: 'frontend' as Role 
-        
+        role: 'frontend' as Role
+
     },
     {
         name: 'JavaScript',
         icon: SiJavascript,
         color: '#f7df1e',
-        role: 'frontend' as Role 
-        
+        role: 'frontend' as Role
+
     },
     {
         name: 'MySQL',
         icon: SiMysql,
-        color: '#4479a1', 
-        role: 'database' as Role 
-        
+        color: '#4479a1',
+        role: 'database' as Role
+
     },
     {
         name: 'MongoDB',
         icon: SiMongodb,
         color: '#47a248',
-        role: 'database' as Role 
-        
+        role: 'database' as Role
+
     },
     {
         name: 'HTML5',
         icon: SiHtml5,
         color: '#e34f26',
-        role: 'frontend' as Role 
+        role: 'frontend' as Role
     },
     {
         name: 'CSS3',
         icon: SiCss3,
         color: '#264de4',
-        role: 'frontend' as Role 
+        role: 'frontend' as Role
     },
     {
         name: 'TailwindCSS',
         icon: SiTailwindcss,
         color: '#06b6d4',
-        role: 'frontend' as Role 
+        role: 'frontend' as Role
     },
     {
         name: 'Docker',
         icon: SiDocker,
         color: '#2496ed',
-        role: 'tools' as Role 
+        role: 'tools' as Role
     },
     {
         name: 'Node.JS',
         icon: SiNodedotjs,
         color: '#339933',
-        role: 'backend' as Role 
+        role: 'backend' as Role
     },
     {
         name: 'Express.JS',
         icon: SiExpress,
         color: '#ffffff',
-        role: 'backend' as Role 
+        role: 'backend' as Role
     },
     {
         name: 'Postman',
         icon: SiPostman,
         color: '#FF6C37',
-        role: 'tools' as Role 
+        role: 'tools' as Role
     },
-   
+
 
 ]
